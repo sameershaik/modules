e6d15956af5bc6e45afcd66005465bbfc3830ad5
sameer aaron <sameer@sameer.AP>
2019-09-11 15:25:17 +0530

    working model of the driver.yet some corrections have to be done.
    Signed-off-by: sameer aaron <sameer@sameer.AP>

--- char_dev.h
+++ char_dev.h
@@ -2,9 +2,9 @@
 #define CHAR_DEV_H_
 #include <linux/cdev.h>
 
-struct char_dev {
+struct my_device {
 	char *message;
-	ssize_t size = 4096;
+	unsigned long size;
 	struct cdev char_device;
 };
 
--- char_driver.c
+++ char_driver.c
@@ -3,37 +3,129 @@
 #include <linux/init.h>
 #include <linux/device.h>
 #include <linux/fs.h>
-
+#include <linux/err.h>
+#include "char_dev.h"
+#include <linux/slab.h>
+#include <linux/uaccess.h>
 
 #define DEVICE "sameer_char"
+#define CLS "my_char"
+#define BUFFER 4096
 
-static int major;
-static int minor;
 static dev_t device_number;
 static struct class *cl = NULL;
+
+static int sameer_open(struct inode *inode, struct file *filp)
+{
+	struct my_device *dev;
+	dev = container_of(inode->i_cdev, struct my_device, char_device);
+
+	// filp->private_data = inode->i_cdev;
+
+	dev->size = BUFFER;
+	dev->message = kmalloc(dev->size, GFP_KERNEL);
+
+	if (dev->message == NULL)
+		return -ENOMEM;
+
+	return 0;
+}
+
+static ssize_t sameer_read(struct file *filp, char __user *buf, size_t count,
+			   loff_t *pos)
+{
+	struct my_device *dev = filp->private_data;
+	int ret = -E2BIG;
+
+	// if (filp->f_pos + count > dev->size)
+	if (*pos + count > dev->size)
+		count = dev->size;
+
+	if (copy_to_user(buf, dev->message, count)) {
+		printk(KERN_WARNING"copying to the userspace failed\n");
+		ret = -EFAULT;
+		goto out;
+	}
+
+	// filp->f_pos = filp->f_pos + count;
+	pos += count;
+	ret = count;
+
+out:
+	return ret;
+}
+
+static ssize_t sameer_write(struct file *filp, const char __user *buf, size_t
+			    count, loff_t *pos)
+{
+	struct my_device *dev = filp->private_data;
+	int ret = -E2BIG;
+
+	// if (filp->f_pos + count > dev->size) {
+	if (*pos + count > dev->size) {
+		printk(KERN_WARNING"writing to this device will cause the\
+		       overflow\n");
+		goto out;
+	}
+
+	if(copy_from_user(dev->message, buf, count)) {
+		printk(KERN_WARNING"Copying from the user space failed\n");
+		ret = -EFAULT;
+		goto out;
+	}
+
+	// filp->f_pos = filp->f_pos + count;
+	pos +=  count;
+	ret = count;
+
+out:
+	return ret;
+}
+
+static int sameer_close(struct inode *inode, struct file *filp)
+{
+	struct my_device *dev = filp->private_data;
+	kfree(dev->message);
+	return 0;
+}
+
+static struct file_operations sops = {
+	.open = sameer_open,
+	.read = sameer_read,
+	.write = sameer_write,
+	.release = sameer_close,
+	.owner = THIS_MODULE
+};
 
 static int start_here(void)
 {
 	int ret;
-
+	struct device *dev = NULL;
+	struct my_device my_device_1 = {0};
+	
 	ret = alloc_chrdev_region(&device_number, 0, 1, DEVICE);
-
 	if (ret != 0) {
 		printk(KERN_WARNING"gettting the device major number failed\n");
-		return -1;
+		return ret;
 	}
 
-	major = MAJOR(device_number);
-	minor = MINOR(device_number);
+	cl = class_create(THIS_MODULE, CLS);
 
-	cl = class_create(THIS_MODULE, "my_char");
-
-	if (cl == NULL)
+	if (IS_ERR(cl))
 		goto fail;
 
-	if (device_create(cl, NULL, device_number, NULL, DEVICE) == NULL)
+	dev = device_create(cl, NULL, device_number, NULL, DEVICE);
+	if (IS_ERR(dev))
 		goto fail1;
 
+	cdev_init(&my_device_1.char_device, &sops);
+
+	ret = cdev_add(&my_device_1.char_device, device_number, 1);
+
+	if (ret) {
+		printk(KERN_ERR"unabe to add the character device\n");
+		goto fail1;
+	}
 	return 0;
 
 fail1:
@@ -43,7 +135,6 @@ fail:
 	unregister_chrdev_region(device_number, 1);
 	return -1;
 }
-
 
 static void end_here(void)
 {
